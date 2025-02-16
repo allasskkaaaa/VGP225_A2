8d2b7d6bcca9e402eb8fca9706c66942ab803a0f
allasskkaaaa <lexiecuvin@gmail.com>
2025-02-16 18:31:27 -0500

    main menu

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
@@ -18,5 +19,20 @@ public class CanvasManager : MonoBehaviour
     public void updateScoreUI()
     {
         if (scoreText != null) scoreText.text = GameManager.gameManager.score.ToString();
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in Editor
+#else
+            Application.Quit(); // Quits the built application
+#endif
     }
 }
